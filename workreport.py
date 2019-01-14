1d0f4da8df55eab22188c30f51d7d6d2ca97c438
Julio Sangrador-Paton <julio.sangrador@schibsted.com>
2019-01-14 17:34:27 +0100

    Initial commit

---
+++ workreport.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+import datetime
+
+# encoding=utf8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+class GmailReporter:
+    def build_report():
+        pass
+
+class GCalendarReporter:
+    def build_report():
+        pass
+
+class TrelloReporter:
+    def build_report():
+        pass
+
+class WorkReporter:
+    def output_report():
+        pass
+
